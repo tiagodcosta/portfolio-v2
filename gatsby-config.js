a8c730c743a008021c7271a97dbf8ce94f5d44e5
Tiago Costa <doriacosta.t@husky.neu.edu>
2017-12-13 10:19:23 -0500

    Add GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,5 +12,11 @@ module.exports = {
                 path: `${__dirname}/src/`,
               },
             },
+            {
+              resolve: `gatsby-plugin-google-analytics`,
+              options: {
+                trackingId: "UA-96777137-1",
+              },
+            },
           ]
 }
